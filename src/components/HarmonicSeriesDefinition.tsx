ba8df8865b3767a312b1cb3f373ba12b47061fc1
David <davidisrawi@gmail.com>
2021-04-25 01:55:05 -0700

    working on better illustrating the concept

--- src/components/HarmonicSeriesDefinition.tsx
+++ src/components/HarmonicSeriesDefinition.tsx
@@ -5,7 +5,10 @@ const HarmonicSeriesDefinition = () => {
         <div className='description'>
             <h3 className='subheader'>The Harmonic Series in Music</h3>
             <p>
-                When a tone is played (not only in music, but also in nature). What we hear is a mix of tone - it starts with a low tone which is the intended one, and it's followed by a series of higher pitch tones that are relative to the initial tone.
+                Imagine you strike a string in a guitar.
+            </p>
+            <p>
+                Now, you are expecting to hear back a tone - the one that corresponds to the string. In reality, what we end up hearing is much more than that - it is a whole collection of tones. The collection starts with a low tone which is the intended one, and it's followed by a series of higher pitch tones that are relative to the initial tone.
             </p>
             <p>
                 Althought this is an infinite series, we can only hear a small subset of these tones.
@@ -15,14 +18,12 @@ const HarmonicSeriesDefinition = () => {
             </p>
 
             <h3 className='subheader'>Why does this happen?</h3>
+            
             <p>
-                Imagine you are playing a guitar.
+                When we hit a string, the string starts bouncing up and down creating a vibration. The tone we are producing is dictated by the frequency at which the string is vibrating. This vibration is creating a series of sine waves - the first sine wave is one big wave bouncing through the whole string, the second one is of two waves, the third of three waves, and so on. All these different vibrations in the string are happening at the same time!
             </p>
             <p>
-                When we hit a string, we produce a tone which is dictated by the frequency at which that string is bouncing up and down. The string is now vibrating, creating a series of sine waves - the first sine wave is one big wave bouncing throug the whole string, the second one is of two waves, the third of three waves and so on.
-            </p>
-            <p>
-                The tones produced by the string come from the wavelength of those sine waves which are all happening together - the first series has one long sine wave, it has the smallest frequency and therefore the lowest tone, and the following frequencies will come from the subsequent sine waves in the series which are getting smaller and smaller, therefore playing higher and higher tones.
+                The tones produced by the string come from the wavelength of those sine waves - the first series has one long sine wave, it has the smallest frequency and therefore the lowest tone, and the following frequencies will come from the subsequent sine waves in the series which are getting smaller and smaller, therefore playing higher and higher tones.
             </p>
         </div>
     )
